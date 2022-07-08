3e816bfaa33335e46ff654039d318efa8ee97cd1
yigit <yigitkazan@hotmail.com>
2022-07-07 21:24:05 -0400

    I added 3 new Classes that are very important.

---
+++ src/FirstClass.java
@@ -0,0 +1,10 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,9 @@
+public class SecondClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Heyy!");
+
+    }
+
+}
